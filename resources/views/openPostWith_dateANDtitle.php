165c27bd0cdf1e491b9b5952da1a4f89fd7bf570
ArmineDan <39946460+ArmineDan@users.noreply.github.com>
2018-11-10 04:03:54 -0800

    Update openPostWith_dateANDtitle.php

--- resources/views/openPostWith_dateANDtitle.php
+++ resources/views/openPostWith_dateANDtitle.php
@@ -1,4 +1,10 @@
+<div id="see_all_images_ns">
 
+    <center>
+        <img src="">
+    </center> 
+
+</div>
 @include('../includes.links')
     <body>
             @include('../includes.facebook_script' )
@@ -20,7 +26,7 @@
                                     <div style="margin-top:24px;">
                                       <a  style="font-size: 17px;" href="{{url($all_last_posts['lang'].'/author/'.$all_last_posts['post'][0]->author_id)}}" target="_blanck">{{$all_last_posts['post'][0]->name .' '.$all_last_posts['post'][0]->lastname}} </a>
                                       <div class="display-table height-100 width-100" style="color:black">
-                                         <div class="vertical-align-bottom display-table-cell icon-social-small ">
+                                         <div class="display-table-cell icon-social-small ">
                                             <div class="separator-line-horrizontal-full bg-deep-pink margin-eleven-tb"></div>
                                              <a href="{{$all_last_posts['post'][0]->faceebook}}" target="_blank"><i class="fa fa-facebook-f"></i></a>
                                              <a href="{{$all_last_posts['post'][0]->twitter}}"  target="_blank"><i class="fa fa-twitter"></i></a>
@@ -71,92 +77,102 @@
                                             </div>
                                     @empty
                                     @endforelse 
-                             </div>
-                                
-                            @endisset
-                       
-                              
-                           <div class="col-md-12 col-sm-12 col-xs-12">
+                             </div>                                
+                            @endisset 
+                            
+                           <div class="col-md-12 col-sm-12 col-xs-12 margin-15px-bottom" >
                                <div class="divider-full bg-medium-light-gray"></div>
                            </div>  
-                           <div class="col-md-6 col-sm-12 col-xs-12 sm-text-center">
+                            <div class="col-md-6 col-sm-12 col-xs-12 sm-text-center">
                                @isset($all_last_posts['tags'])
                                  <div class="tag-cloud margin-20px-bottom">
                                    @for ($i = 0; $i < count($all_last_posts['tags']); $i++)
-                                 <a href="{{url( $all_last_posts['lang'].'/tags/'.$all_last_posts['tags'][$i])}}" >{{$all_last_posts['tags'][$i]}}</a>   
+                                  <a href="{{url( $all_last_posts['lang'].'/tags/'.$all_last_posts['tags'][$i])}}" >{{$all_last_posts['tags'][$i]}}</a>   
                                    @endfor                                                       
                                </div>  
-                               @endisset
-                               
-                           </div>  
-                           @isset($all_last_posts['comments'] )
-                           <div class="col-md-12 col-sm-12 col-xs-12 no-padding">
-                                <ul class="blog-comment">
-                                        @forelse ($all_last_posts['comments'] as $item)
-                                    <li> 
-                                        <div class="display-table width-100">                                               
-                                                <div class="padding-40px-left display-table-cell vertical-align-top last-paragraph-no-margin xs-no-padding-left xs-display-block">
-                                                    <span class="text-extra-dark-gray text-uppercase alt-font font-weight-600 text-small">{{$item->c_name}}</span>
-                                                        
-                                                    <div class="text-small text-medium-gray text-uppercase margin-10px-bottom"><?php echo  date('Y-m-d', strtotime($item->created_at)) ?></div>
-                                                    <p style="font-size:12px">{{$item->body}}</p>                      
-                                                </div>
-                                            </div>                                            
-                                        </li>
-                                    @empty                                      
-                                    @endforelse                                
-                                 </ul>
+                               @endisset                               
                              </div> 
-                           @endisset                        
-                          
-                             <div class="col-md-12 col-sm-12 col-xs-12 text-center">
-                             <button class="btn btn-dark-gray btn-small margin-15px-top" id="com_btn" >{{trans('text.comment')}}</button>
-                                </div>
-             
-                                   <div class="row"  id="com_form"  style="display:none">  
-                                        <div class="col-md-12 col-sm-12 col-xs-12 text-center">                  
-                                    <div class="col-md-12 col-sm-12 col-xs-12 margin-eight-top" >
-                                        <div class="divider-full bg-medium-light-gray"></div>
-                                    </div>
-            
-                                    <div class="col-md-12 col-sm-12 col-xs-12 margin-lr-auto text-center margin-80px-tb sm-margin-50px-tb xs-margin-30px-tb">
-                                        <div class="position-relative overflow-hidden width-100">
-                                            <span class="text-small text-outside-line-full alt-font font-weight-600 text-uppercase text-extra-dark-gray">Թողնել կարծիք</span>
-                                        </div>
-                                    </div>
-                                  
-                                    <div class="col-md-6 col-sm-12 col-xs-12">
-                                        <input type="text" placeholder="Անուն *" class="medium-input">
-                                    </div>
-                                    <div class="col-md-6 col-sm-12 col-xs-12">
-                                        <input type="text" placeholder="Էլ.-հասցե *" class="medium-input">
-                                    </div>
-                                    <div class="col-md-12 col-sm-12 col-xs-12">
-                                        <textarea placeholder="Ձեր կարծիքը..." rows="8" class="medium-textarea" ></textarea>
-                                    </div>
-                                    <div class="g-recaptcha col-md-12 col-sm-12 col-xs-12 text-center" data-sitekey="6LebWHkUAAAAAMW2otYGKxyP0Q-p4_jJpvm4Q3QV"></div>
+
+                             @isset($all_last_posts['comments'] )
+                           <div class="col-md-12 col-sm-12 col-xs-12 no-padding">
+                                <ul class="blog-comment" id="narek_comment_ul">
+                                        @foreach ($all_last_posts['comments'] as $key => $value )
+                                    @if ($key <5)
+                                    <li> 
+                                            <div class="display-table width-100">                                               
+                                                    <div class="padding-40px-left display-table-cell vertical-align-top last-paragraph-no-margin xs-no-padding-left xs-display-block">
+                                                        <span class="text-extra-dark-gray text-uppercase alt-font font-weight-600 text-small">{{$value->c_name}}</span>
+                                                            
+                                                        <div class="text-small text-medium-gray text-uppercase margin-10px-bottom"><?php echo  date('Y-m-d', strtotime($value->created_at)) ?></div>
+                                                        <p style="font-size:12px">{{$value->body}}</p>                      
+                                                    </div>
+                                                </div>                                            
+                                            </li>
+                                    @endif                                 
+                                                                
+                                    @endforeach                               
+                                 </ul>
+                                 <br/>
                                 
-                                    <div class="col-md-12 col-sm-12 col-xs-12 text-center">
-                                        <button  id="send_comment" class="btn btn-dark-gray btn-small margin-15px-top" type="submit">{{trans('text.send')}}</button>
-                                    </div>
-                               
-
-                                   <div class="col-md-12 col-sm-12 col-xs-12 margin-eight-top" >
-                                        <div class="divider-full bg-medium-light-gray"></div>
-                                    </div>
-
-
-                                </div>
-                         </div>
+                                 <div id="narek_db_comments"></div>                                
+                                 <center id="more_n" class="{{count($all_last_posts['comments'])-5}}" 
+                                 style="display:{{count($all_last_posts['comments'])-5<0 ? 'none':''}}" ><i class="fa fa-plus" style="font-size: 25px;padding-top: 30px;cursor: pointer" id="ns_click_plus_com_btn"></i></center>
+                                  </div>
+                           @endisset                     
                           
-                       </main>
-                       
                      
+                       <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                            <button class="btn btn-dark-gray btn-small margin-15px-top" id="com_btn" >{{trans('text.comment')}}</button>
+                              <div class="row"  id="com_form"  style="display:none">  
+                                <div class="col-md-12 col-sm-12 col-xs-12 text-center">                  
+                                   <div class="col-md-12 col-sm-12 col-xs-12 margin-eight-top" >
+                                       <div class="divider-full bg-medium-light-gray"></div>
+                                   </div>
+           
+                                   <div class="col-md-12 col-sm-12 col-xs-12 margin-lr-auto text-center margin-80px-tb sm-margin-50px-tb xs-margin-30px-tb">
+                                       <div class="position-relative overflow-hidden width-100">
+                                           <span class="text-small text-outside-line-full alt-font font-weight-600 text-uppercase text-extra-dark-gray">Թողնել կարծիք</span>
+                                       </div>
+                                   </div>
+                                 
+                                   <div class="col-md-6 col-sm-12 col-xs-12">
+                                       <input type="text" placeholder="Անուն *" class="medium-input" id="name_comment_inp_ns">
+                                   </div>
+                                   <div class="col-md-6 col-sm-12 col-xs-12">
+                                       <input type="email" placeholder="Էլ.-հասցե *" class="medium-input" id="mail_comment_inp_ns">
+                                   </div>
+                                   <div class="col-md-12 col-sm-12 col-xs-12">
+                                       <textarea placeholder="Ձեր կարծիքը..." rows="8" class="medium-textarea" id="opinion_comment_inp_ns"></textarea>
+                                   </div>
+
+                                   <input type="hidden" id="hidden_id_comments_narek" value="{{$all_last_posts['id']}}">
+                                  
+                                  
+                                   <div class="g-recaptcha col-md-12 col-sm-12 col-xs-12 text-center" data-sitekey="6LebWHkUAAAAAMW2otYGKxyP0Q-p4_jJpvm4Q3QV"></div>
+                               
+                                   <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                                       <button  id="send_comment" class="btn btn-dark-gray btn-small margin-15px-top" type="submit">{{trans('text.send')}}</button>
+
+                                   </div>
+                              
+
+                                  <div class="col-md-12 col-sm-12 col-xs-12 margin-eight-top" >
+                                       <div class="divider-full bg-medium-light-gray"></div>
+                                   </div>
+
+
+                               </div>
+                             </div>
+                        </div>
+                    </main>
+                   
+             
                         @isset($all_last_posts['same_posts'])
                         <div class="col-md-12 col-sm-12 col-xs-12 no-padding">
                                 <div class="col-md-12 col-sm-12 col-xs-12 margin-lr-auto text-center margin-80px-tb sm-margin-50px-tb xs-margin-30px-tb">
                                     <div class="position-relative overflow-hidden width-100">
                                         <span class="text-small text-outside-line-full alt-font font-weight-600 text-uppercase text-extra-dark-gray">{{trans('text.same_posts')}}</span>
+
                                     </div>
                                 </div>
  
@@ -165,13 +181,13 @@
                                 <div class="col-md-3 col-sm-6 col-xs-12 last-paragraph-no-margin sm-margin-50px-bottom xs-margin-30px-bottom wow fadeIn" style="visibility: hidden; animation-name: none;">
                                      <div class="blog-post blog-post-style1 xs-text-center">
                                          <div class="blog-post-images overflow-hidden margin-25px-bottom sm-margin-20px-bottom">
-                                             <a href="{{url( $all_last_posts['lang'].'/post/'.$all_last_posts['same_posts'][$i][0]->date.'/'.$all_last_posts['same_posts'][$i][0]->title)}}">
+                                             <a href="{{url( $all_last_posts['lang'].'/'.$all_last_posts['folder'].'/'.$all_last_posts['same_posts'][$i][0]->date.'/'.$all_last_posts['same_posts'][$i][0]->title)}}">
                                                  <img src="/{{$all_last_posts['same_posts'][$i][0]->img}}" alt="" data-no-retina="">
                                              </a>
                                          </div>
                                          <div class="post-details">
                                              <span class="post-author text-extra-small text-medium-gray text-uppercase display-block margin-10px-bottom xs-margin-5px-bottom">{{$all_last_posts['same_posts'][$i][0]->date}} |  <a href="#" class="text-medium-gray">{{substr($all_last_posts['same_posts'][$i][0]->name, 0, 2) .'. '. $all_last_posts['same_posts'][$i][0]->lastname}}</a></span>
-                                         <a href="{{url( $all_last_posts['lang'].'/post/'.$all_last_posts['same_posts'][$i][0]->date.'/'.$all_last_posts['same_posts'][$i][0]->title)}}"  class="post-title text-medium text-extra-dark-gray width-90 display-block sm-width-100">{{$all_last_posts['same_posts'][$i][0]->title}}</a>
+                                         <a href="{{url( $all_last_posts['lang'].'/'.$all_last_posts['folder'].'/'.$all_last_posts['same_posts'][$i][0]->date.'/'.$all_last_posts['same_posts'][$i][0]->title)}}"  class="post-title text-medium text-extra-dark-gray width-90 display-block sm-width-100">{{$all_last_posts['same_posts'][$i][0]->title}}</a>
                                              <div class="separator-line-horrizontal-full bg-medium-light-gray margin-20px-tb sm-margin-15px-tb"></div>
                                              <p class="width-90 xs-width-100">{{$all_last_posts['same_posts'][$i][0]->short_text}}</p>
                                          </div>
@@ -190,17 +206,40 @@
            <!-- end blog content section -->  
     
            <section class="no-padding_top">
-                <div class="container">
-                   
-                       
-                    </div>
-                
+                <div class="container"></div>
             </section>
-   
-       
+  
 @include('includes.footer')
 <!-- start scroll to top -->
 <a class="scroll-top-arrow" href="javascript:void(0);"><i class="ti-arrow-up"></i></a>
 @include('../includes.scripts')
+<script>
+    $("#html_div img").click(function(){
+        $("#see_all_images_ns").css("display","block");
+        $("#see_all_images_ns img").attr("src",$(this).attr("src"))
+    })
+
+    $("#see_all_images_ns").click(function(){
+        $("#see_all_images_ns").css("display","none");
+    })
+
+    $("#go_db_btn_comment").click(function(){
+        $.post(
+            "/php/set_comment.php",
+            {
+                name: $("#name_comment_inp_ns").val(),
+                mail: $("#mail_comment_inp_ns").val(),
+                opinion: $("#opinion_comment_inp_ns").val(),
+                id: $("#hidden_id_comments_narek").val()
+            },
+            function(result){
+                $("#comment_answer_ns").val(result);
+            }
+        )
+        $("#name_comment_inp_ns").val(""),
+        $("#mail_comment_inp_ns").val(""),
+        $("#opinion_comment_inp_ns").val("")
+    })    
+</script>
 </body>
 </html>
